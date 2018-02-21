f9edcf8478fb9832b9cb6deeafe065ab74cf75fb
ioanachi <ioana_chichi@outlook.com>
2018-02-21 13:33:40 +0200

    popup

--- src/js/mainContainer.js
+++ src/js/mainContainer.js
@@ -1,23 +1,33 @@
 var dropdown = document.querySelectorAll('.dropdown');
-var dropdownArray = Array.prototype.slice.call(dropdown,0);
-dropdownArray.forEach(function(el){
-	var button = el.querySelector('a[data-toggle="dropdown"]'),
-			menu = el.querySelector('.dropdown-menu');
+var dropdownArray = Array.prototype.slice.call(dropdown, 0);
+dropdownArray.forEach(function(el) {
+  var button = el.querySelector('a[data-toggle="dropdown"]'),
+    menu = el.querySelector('.dropdown-menu');
 
-	button.onclick = function(event) {
-		if(!menu.hasClass('show')) {
-			menu.classList.add('show');
-			menu.classList.remove('hide');
-			event.preventDefault();
-		}
-		else {
-			menu.classList.remove('show');
-			menu.classList.add('hide');
-			event.preventDefault();
-		}
-	};
+  button.onclick = function(event) {
+    if (!menu.hasClass('show')) {
+      menu.classList.add('show');
+      menu.classList.remove('hide');
+      event.preventDefault();
+    } else {
+      menu.classList.remove('show');
+      menu.classList.add('hide');
+      event.preventDefault();
+    }
+  };
 })
 
 Element.prototype.hasClass = function(className) {
-    return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
+  return this.className && new RegExp("(^|\\s)" + className + "(\\s|$)").test(this.className);
 };
+var popupButton = document.getElementById('searchAndPopup');
+var popupContainer = document.getElementById('popup');
+
+
+popupButton.onclick = function(event) {
+  console.log(typeof button, "popupButton MErge");
+
+  popupContainer.style.display = "inline-block !important";
+  event.preventDefault();
+
+}
