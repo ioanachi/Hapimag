10095f31969c18e88130d944c08c73a5c0b99815
ioanachi <ioana_chichi@outlook.com>
2018-02-21 10:49:51 +0200

    menu

--- src/app.js
+++ src/app.js
@@ -1,14 +1,5 @@
 import "../index.html";
-import 'react';
-import 'react-dom';
 
-import 'react-datepicker/dist/react-datepicker.css';
-
-
-
-import 'reactcss';
-import 'react-tabs';
-import '../node_modules/react-tabs/style/react-tabs.css';
-
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import "./scss/main.scss";
 import "./js/script.js";
