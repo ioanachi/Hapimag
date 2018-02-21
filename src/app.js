ae8972a8947f934f5c71ccd840efc5b3d598390e
ioanachi <ioana_chichi@outlook.com>
2018-02-21 09:56:45 +0200

    added webpack

---
+++ src/app.js
@@ -0,0 +1,14 @@
+import "../index.html";
+import 'react';
+import 'react-dom';
+
+import 'react-datepicker/dist/react-datepicker.css';
+
+
+
+import 'reactcss';
+import 'react-tabs';
+import '../node_modules/react-tabs/style/react-tabs.css';
+
+import "./scss/main.scss";
+import "./js/script.js";
